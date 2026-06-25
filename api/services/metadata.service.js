0b77ec6479d1ca2452e6f9f69a5c33e3468b9491
LucasTheiss1 <lucasptheiss@gmail.com>
2026-06-25 19:46:15 +0100

    Create test service to GET metadata from a single mp3 file

---
+++ api/services/metadata.service.js
@@ -0,0 +1,29 @@
+const mm = require("music-metadata");
+const fs = require("fs");
+const path = require("path");
+
+async function getTrackMetadata(filePath) {
+  const metadata = await mm.parseFile(filePath);
+  const picture = metadata.common.picture?.[0];
+
+  let coverUrl = null;
+
+  if (picture) {
+    const extension = picture.format.split("/")[1] || "jpg";
+    const coverFileName = `cover.${extension}`;
+    const coverPath = path.join(__dirname, "..", "public", "covers", coverFileName);
+
+    fs.writeFileSync(coverPath, picture.data);
+    coverUrl = `/covers/${coverFileName}`;
+  }
+
+  return {
+    title: metadata.common.title || "Unknown Title",
+    artist: metadata.common.artist || "Unknown Artist",
+    album: metadata.common.album || "Unknown Album",
+    duration: metadata.format.duration || null,
+    cover: coverUrl
+  };
+}
+
+module.exports = { getTrackMetadata };
